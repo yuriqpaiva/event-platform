9ec3cdebcf1e1e8f0f9684da75d3b36c282ddf08
Yuri <yuriqpaiva@gmail.com>
2022-06-22 19:19:22 -0300

    Structuring application Event (HTML, CSS, GraphQL API Calls to get lessons)

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,30 @@
-import { gql, useQuery } from '@apollo/client';
+// import { gql, useQuery } from '@apollo/client';
 
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`;
+import { Event } from "./pages/Event";
 
-interface Lesson {
-  id: string;
-  title: string;
-}
+// const GET_LESSONS_QUERY = gql`
+//   query {
+//     lessons {
+//       id
+//       title
+//     }
+//   }
+// `;
+
+// interface Lesson {
+//   id: string;
+//   title: string;
+// }
 
 function App() {
-  // Using Apollo's hook to query GraphQL from our Backend 
+  // Using Apollo's hook to query GraphQL from our Backend
   // We defined our client and provide him with Apollo's Provider at main.tsx file
-  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY);
-
-  console.log(data);
+  // const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY);
 
   return (
-    <ul>
-      {data?.lessons.map((lesson) => {
-        return <li key={lesson.id}>{lesson.title}</li>;
-      })}
-    </ul>
+    <div>
+      <Event />
+    </div>
   );
 }
 
