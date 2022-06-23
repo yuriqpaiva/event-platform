a21c9c6b30c7f8774d288f95355ce6c0e77f38ef
Yuri <yuriqpaiva@gmail.com>
2022-06-23 15:15:28 -0300

    Adding video styling and router

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,16 @@
-// import { gql, useQuery } from '@apollo/client';
-
-import { Event } from "./pages/Event";
-
-// const GET_LESSONS_QUERY = gql`
-//   query {
-//     lessons {
-//       id
-//       title
-//     }
-//   }
-// `;
-
-// interface Lesson {
-//   id: string;
-//   title: string;
-// }
+import { ApolloProvider } from '@apollo/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Router } from './components/Router';
+import { client } from './lib/apollo';
+import { Event } from './pages/Event';
 
 function App() {
-  // Using Apollo's hook to query GraphQL from our Backend
-  // We defined our client and provide him with Apollo's Provider at main.tsx file
-  // const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY);
-
   return (
-    <div>
-      <Event />
-    </div>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ApolloProvider>
   );
 }
 
--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,6 +1,7 @@
 import { CheckCircle, Lock } from 'phosphor-react';
 import { isPast, format } from 'date-fns';
 import ptBr from 'date-fns/locale/pt-BR';
+import { Link } from 'react-router-dom';
 
 interface LessonProps {
   title: string;
@@ -18,15 +19,15 @@ export function Lesson({ title, slug, availableAt, type }: LessonProps) {
 
     // Characters that are between single quotes will be ignored by date fns
     "EEEE' • 'd' de 'MMMM' • 'k'h'mm",
-    
+
     { locale: ptBr }
   );
 
   return (
-    <a href="#">
+    <Link to={`/event/lesson/${slug}`} className="group">
       <span className="text-gray-300">{availableDateFormatted}</span>
 
-      <div className="rounded border border-gray-500 p-4 mt-2">
+      <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500">
         <header className="flex items-center justify-between">
           {isLessonAvailable ? (
             <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
@@ -46,6 +47,6 @@ export function Lesson({ title, slug, availableAt, type }: LessonProps) {
 
         <strong className="text-gray-200 mt-5 block">{title}</strong>
       </div>
-    </a>
+    </Link>
   );
 }
